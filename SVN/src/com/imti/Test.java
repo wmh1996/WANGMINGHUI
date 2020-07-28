f1674456c2b0c99bf8b4d8269db69bdbfc4c7ec6
wmh1996 <326864231@qq.com>
2020-07-28 13:08:50 +0800

    新增

---
+++ SVN/src/com/imti/Test.java
@@ -0,0 +1,19 @@
+package com.imti;
+/**@文件名: Test.java
+ * @类功能说明: 
+ * @作者: WangMinghui
+ * @Email: 326864231@qq.com
+ * @日期: 2020年7月28日下午12:54:27
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: WangMinghui</li> 
+ * 	 <li>日期: 2020年7月28日下午12:54:27</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
